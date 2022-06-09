637bbcfe7c1038e0212fdec043a0b8efcd538fe4
Jenna Scholl <bin19005@byui.edu>
2022-06-09 13:24:10 -0600

    edited login

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Counter from './Counter.js';
+import Login from "./Login.js";
 import SettingsScreen from './SettingsScreen.js';
 import Home from './Home.js';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
@@ -12,47 +13,59 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 // import Icons from "./Icons";
 const Tab = createMaterialBottomTabNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName='Home'
-        activeColor='white'
-        barStyle={{ backgroundColor: 'green' }}
-      >
-        <Tab.Screen
-          name='Home'
-          component={Home}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='home' color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Step Counter'
-          component={Counter}
-          options={{
-            tabBarLabel: 'Step Counter',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='watch' color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Settings'
-          component={SettingsScreen}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarIcon: ({ color }) => (
-              <FontAwesome name='gear' color={color} size={26} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+export default function App(props) {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  if (userLoggedIn)
+  {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName='Home'
+          activeColor='white'
+          barStyle={{ backgroundColor: 'green' }}
+        >
+          <Tab.Screen
+            name='Home'
+            component={Home}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name='home' color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name='Step Counter'
+            component={Counter}
+            options={{
+              tabBarLabel: 'Step Counter',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name='watch' color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name='Settings'
+            component={SettingsScreen}
+            options={{
+              tabBarLabel: 'Settings',
+              tabBarIcon: ({ color }) => (
+                <FontAwesome name='gear' color={color} size={26} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+    
+  } else {
+    return (
+      <View>
+        <Login/>
+      </View>
+    )
+  }
 
 
 }
@@ -61,3 +74,4 @@ export default function App() {
 const styles = StyleSheet.create({
   
 });
+
